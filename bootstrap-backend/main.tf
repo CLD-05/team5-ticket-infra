d62b6d8291e1a23891948b2984bb648cf0efd3d3
UniqueOnion <dlwodns5841@naver.com>
2026-06-09 10:31:58 +0900

    feat: 프로젝트 가이드라인에 따라 DynamoDB 대신 S3 네이티브 락(use_lockfile) 사용하도록 백엔드 수정

--- bootstrap-backend/main.tf
+++ bootstrap-backend/main.tf
@@ -31,14 +31,3 @@ resource "aws_s3_bucket_versioning" "terraform_state_prod" {
     status = "Enabled"
   }
 }
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "team5-ticket-tfstate-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
--- envs/prod/platform-addons/backend.tf
+++ envs/prod/platform-addons/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "team5-ticket-tfstate-prod"
-    key            = "prod/platform-addons/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "team5-ticket-tfstate-lock"
-    encrypt        = true
+    bucket       = "team5-ticket-tfstate-prod"
+    key          = "prod/platform-addons/terraform.tfstate"
+    region       = "ap-northeast-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
