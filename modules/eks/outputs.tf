fc153308eb62b97a643bbe6adfd646b7236e5b30
jihyeon <its.jihyeon@gmail.com>
2026-06-09 11:50:26 +0900

    feat: 환경별 노드 용량 자동 분기, 보안 규격 최적화

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,1 +1,27 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
 
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "lbc_iam_role_arn" {
+  value = aws_iam_role.lbc.arn
+}
+
+output "eso_iam_role_arn" {
+  value = aws_iam_role.eso.arn
+}
+
+output "external_dns_iam_role_arn" {
+  value = aws_iam_role.external_dns.arn
+}
