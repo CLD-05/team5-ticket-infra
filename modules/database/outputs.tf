b966d10edf80301ac2cb0dd40e12117528731a52
sihyun <sini5971@gmail.com>
2026-06-09 11:46:58 +0900

    feat: RDS 설정 변수화 및 비밀번호 생성 적용

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -7,3 +7,25 @@ output "db_address" {
   value       = aws_db_instance.main.address
   description = "The database address"
 }
+
+output "db_name" {
+  value       = aws_db_instance.main.db_name
+  description = "The database name"
+}
+
+output "db_username" {
+  value       = aws_db_instance.main.username
+  description = "The database master username"
+  sensitive   = true
+}
+
+output "db_password" {
+  value       = random_password.db.result
+  description = "The generated database master password"
+  sensitive   = true
+}
+
+output "db_security_group_id" {
+  value       = aws_security_group.rds.id
+  description = "The RDS security group ID"
+}
