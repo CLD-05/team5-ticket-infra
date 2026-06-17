361135113a2dcd52d4eccb5c2c5db399b891ad90
UniqueOnion <dlwodns5841@naver.com>
2026-06-17 15:23:47 +0900

    fix: SQS 모듈의 FIFO 여부에 따른 큐 이름 조건 처리

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -1,10 +1,10 @@
 resource "aws_sqs_queue" "booking_dlq" {
-  name                      = "team5-${var.environment}-${var.dlq_name}.fifo"
+  name                      = var.fifo_queue ? "team5-${var.environment}-${var.dlq_name}.fifo" : "team5-${var.environment}-${var.dlq_name}"
   message_retention_seconds = var.dlq_message_retention_seconds
   fifo_queue                = var.fifo_queue
 
   tags = {
-    Name        = "team5-${var.environment}-${var.dlq_name}.fifo"
+    Name        = var.fifo_queue ? "team5-${var.environment}-${var.dlq_name}.fifo" : "team5-${var.environment}-${var.dlq_name}"
     Team        = "team5"
     Environment = var.environment
     Project     = var.project_name
@@ -13,12 +13,12 @@ resource "aws_sqs_queue" "booking_dlq" {
 }
 
 resource "aws_sqs_queue" "booking_queue" {
-  name                        = "team5-${var.environment}-${var.queue_name}.fifo"
+  name                        = var.fifo_queue ? "team5-${var.environment}-${var.queue_name}.fifo" : "team5-${var.environment}-${var.queue_name}"
   delay_seconds               = var.delay_seconds
   message_retention_seconds   = var.message_retention_seconds
   visibility_timeout_seconds  = var.visibility_timeout_seconds
   fifo_queue                  = var.fifo_queue
-  content_based_deduplication = var.content_based_deduplication
+  content_based_deduplication = var.fifo_queue ? var.content_based_deduplication : null
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.booking_dlq.arn
@@ -26,7 +26,7 @@ resource "aws_sqs_queue" "booking_queue" {
   })
 
   tags = {
-    Name        = "team5-${var.environment}-${var.queue_name}.fifo"
+    Name        = var.fifo_queue ? "team5-${var.environment}-${var.queue_name}.fifo" : "team5-${var.environment}-${var.queue_name}"
     Team        = "team5"
     Environment = var.environment
     Project     = var.project_name
