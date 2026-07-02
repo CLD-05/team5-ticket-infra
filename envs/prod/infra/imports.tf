63d88c36a47937bd973529fba504d4f2a10c4a41
UniqueOnion <dlwodns5841@naver.com>
2026-07-02 17:39:54 +0900

    chore(eks): remove runner from imports.tf

--- envs/prod/infra/imports.tf
+++ envs/prod/infra/imports.tf
@@ -1,13 +1,4 @@
-# Temporary import blocks to reconcile state with existing AWS resources.
-# These node groups already exist in AWS but were lost from Terraform state
-# due to a partial apply failure. After a successful apply, remove this file.
-
 import {
   to = module.eks.module.eks.module.eks_managed_node_group["app"].aws_eks_node_group.this[0]
   id = "team5-prod-eks:team5-prod-eks-app-ng"
-}
-
-import {
-  to = module.eks.module.eks.module.eks_managed_node_group["runner"].aws_eks_node_group.this[0]
-  id = "team5-prod-eks:team5-prod-eks-runner-ng"
 }
